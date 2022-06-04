5bba11ac2d61ab787c69bd28ef2285f490e7f609
adamglasser <83619802+adamglasser@users.noreply.github.com>
2022-06-03 22:41:22 -0600

    start work on home page, add text, buttons, etc...

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavLink = ({ to, label }) => (<>
 <li><Link to={to} className="nav-link px-2 link-dark">{label}</Link></li>
 </>)
 
-const Header = () => <><div className="container">
+const Header = () => {
+    const navigate = useNavigate();
+
+    return(<><div className="container">
 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-  <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-    <h5 className="fw-bold">LOGO</h5>
-  </a>
+  <div onClick={() => navigate('/')} style={{cursor: 'pointer'}} className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <h3 className="fw-bold">🚀</h3>
+  </div>
 
   <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-    <NavLink to={'/'} label="Home" className="nav-link px-2 link-dark" />
     <NavLink to="Work" label="Work" className="nav-link px-2 link-dark" />
     <NavLink to="About" label="About" className="nav-link px-2 link-dark" />
     <NavLink to="Resume" label="Resume" className="nav-link px-2 link-dark" />
@@ -21,6 +23,6 @@ const Header = () => <><div className="container">
     <button type="button" className="btn btn-outline-dark fw-bold rounded-0 border border-2 border-dark px-4"><small>Let{`'`}s chat</small></button>
   </div>
 </header>
-</div></>
+</div></>)}
 
 export default Header;
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,10 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/header";
 
 const Home = () => {
-    const t = '1'
+    const navigate = useNavigate();
     return (<>
 <div style={{minHeight: '100vh'}} className="bg-white">
         <Header />
+        <div className="container position-relative py-5">
+          <div class="row justify-content-md-start justify-content-center">
+            <div className="col-md-6 col-sm-8 order-md-1 order-2 d-flex flex-column justify-content-between mt-4 pt-2 text-md-start text-center">
+              <div class="mb-md-5 pb-xl-5 mb-4">
+
+
+                <h1 class="display-2 mb-3 pb-3">
+                  Hi! I Am <br /> <span class="text-purple fw-bold">Adam Glasser</span>
+                </h1>
+                <p className="lead mb-4">Designing engaging digital experiences for for <span className="fw-bold">4 years</span> as a solutions architect</p>
+                <div class="d-md-flex align-items-md-start">
+                  <div onClick={() => navigate('/contact')} class="btn btn-lg btn-orange text-white rounded-0 flex-shrink-0 me-md-4 mb-md-0 mb-sm-4 mb-3">Hire me</div>
+                  <div onClick={() => navigate('/contact')} class="btn btn-white btn-lg rounded-0 text-dark me-md-4 mb-md-0 mb-sm-4 mb-3 fw-bold">Work</div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="col-sm-6 col-sm-6 col-9 order-md-2 order-1">
+              <lottie-player class="mx-auto" src="assets/json/animation-digital-agency.json" background="transparent" speed="1" loop="" autoplay=""></lottie-player>
+            </div>
+          </div>
+        </div>
 </div>
 </>)}
 
