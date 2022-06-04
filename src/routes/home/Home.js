3920a0aca541f13767260c44f8f8e81cc9083d5e
adamglasser <83619802+adamglasser@users.noreply.github.com>
2022-06-04 12:28:21 -0600

    declutter and fix busy-ness

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -6,20 +6,21 @@ const MainSlice = () => {
     const navigate = useNavigate();
     return (
         <>
-            <div className="container py-5 h-100 d-flex flex-column flex-grow-1">
+            <div className="container-fluid  py-5 h-100 d-flex flex-column flex-grow-1">
                 <div className="row justify-content-md-start justify-content-center my-auto">
-                    <div className="col-md-6 col-sm-8 order-md-1 order-2 d-flex flex-column justify-content-between mt-4 pt-2 text-md-start text-left">
-                        <div className="pb-xl-5 bg-black p-4 rounded shadow">
+                    <div className="col-auto order-md-1 order-2 d-flex flex-column justify-content-between mt-4 pt-2 text-md-start text-left">
+                        <div className="p-5 w-100 bg-black rounded rounded-5">
 
 
-                            <h1 className="display-1 mb-3 pb-3 fw-bold text-light">
+                            <h1 className="display-2 mb-3 pb-3 fw-bold text-light">
                                 Adam Glasser
                             </h1>
                             <h3 className="text-white"> is a {' '}
                                 <span style={{ color: '#ff9fd6' }}>developer</span>, {' '}
                                 <span style={{ color: '#61dafb' }}>marketer</span>, {' '}
-                                <span style={{ color: '#4ca96d' }}>creator</span>, {' '} and  {' '}
-                                <span style={{ color: '#ffd049' }}>problem solver</span>.
+                                <span style={{ color: '#4ca96d' }}>creator</span>,{' '} 
+                                <br />
+                                {' '} and <span style={{ color: '#ffd049' }}>problem solver</span>.
                             </h3>
 
                             <div className="d-md-flex align-items-md-start mt-5">
@@ -38,7 +39,7 @@ const MainSlice = () => {
 
 const Home = () => {
     return (<>
-        <div style={{ minHeight: '100vh', backgroundImage: `url(${Blobs})`, backgroundSize: 'cover', backgroundPosition: 'left' }} className="bg-black d-flex flex-column">
+        <div style={{ minHeight: '100vh', backgroundImage: `url(${Blobs})`, backgroundSize: 'cover', backgroundPosition: 'bottom' }} className="bg-black d-flex flex-column">
             <Header />
             <MainSlice />
 
