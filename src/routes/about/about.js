8f48c63c3adb74ee41624c9946b17e4af4a657d1
adamglasser <83619802+adamglasser@users.noreply.github.com>
2022-06-23 00:44:54 -0600

    can't get the design with the illustration quite nailed down

--- src/routes/about/about.js
+++ src/routes/about/about.js
@@ -1,30 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/header/header';
 import AdamPeep from './assets/AG-Open-Peep.svg'
+import './styles/About.scss'
 
-const About = () => (
-    <div className="bg-white h-100 d-flex flex-column" style={{ minHeight: '100vh' }}>
-        <Header textClassName={'dark-nav-link'} />
-        <div className='container d-flex flex-column py-lg-5'>
-            <div className='row w-100 mx-auto'>
-                <div className='col-auto col-lg-5 d-flex flex-column'>
-                    <div className='my-auto'>
-                        <img src={AdamPeep} className="img-fluid" alt="Adam Glasser" />
-                        <p className='text-muted text-center mt-1'><small>*Illustration not to scale</small></p>
-                    </div>
+
+const About = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className="funky-pattern bg-light h-100 d-flex flex-column adam-wrapper">
+                <Header textClassName={'dark-nav-link'} />
+                <div className='mx-auto intro-text-box px-5 col col-lg-8 col-xl-11 col-xxl-6'>
+                    <h1 className='mb-3 col-xl-10 ms-auto ms-xxl-0 text-slate fw-bolder text-leckerli'>Hey<br></br> There!</h1>
                 </div>
-                <div className='col-auto col-lg-7 d-flex flex-column'>
-                    <div className='my-auto'>
-                        <h1 className='display-1 text-slate fw-bolder'>Hey There!</h1>
-                        <h3 className='text-slate my-3'>Thanks for checking out my site, you can learn a little more about me below
-                        </h3>
-                        <p className='text-slate lead'>I am currently the Senior Solutions Architect at PFL where I specialize in creating engaging, responsive, and visually striking digital experiences and SAAS applications for all kinds of businesses, from enterprise corporations to budding startups.</p>
-                        <p className='text-slate lead'>I have a wide range of technical expertise, from constructing point solutions to solve discrete data processing or software integration issues, to managing and consulting on digital marketing initiatives, to creating full-stack applications using a range of technologies.  Whether you're looking to improve your digital marketing KPIs, or construct a new product or service to support your brand, I can probably help.
-                        </p>
-                    </div>
-                </div>
+                <img className="adam-peep" src={AdamPeep} alt="Adam Glasser" />
             </div>
-        </div>
-    </div>
-)
+        </>
+    )
+}
 
 export default About;
